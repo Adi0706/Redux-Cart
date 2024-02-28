749bb4e53b2f78a8146d9b7cad5c28177630f019
Aditya Bhattacharjee <adityab76@gmail.com>
2024-02-28 13:31:14 +0530

    updated code

--- src/Features/cartSlice.js
+++ src/Features/cartSlice.js
@@ -3,7 +3,7 @@ import ProductData from "../ProductData";
 
 const initialState = {
   cart: [],
-  items:ProductData,
+  items: ProductData,
   totalPrice: 0,
   totalQuantity: 0,
 };
@@ -13,15 +13,14 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-        const { id } = action.payload;
-        const existingItemIndex = state.cart.findIndex((item) => item.id === id);
-        if (existingItemIndex >= 0) {
-          state.cart[existingItemIndex].productQuantity += 1;
-        } else {
-          state.cart.push({ ...action.payload, productQuantity: 1 });
-        }
-      },
-      
+      const { id } = action.payload;
+      const existingItemIndex = state.cart.findIndex((item) => item.id === id);
+      if (existingItemIndex >= 0) {
+        state.cart[existingItemIndex].productQuantity += 1;
+      } else {
+        state.cart.push({ ...action.payload, productQuantity: 1 });
+      }
+    },
     getCartTotal: (state) => {
       let { totalQuantity, totalPrice } = state.cart.reduce(
         (cartTotal, cartItem) => {
@@ -39,20 +38,37 @@ export const cartSlice = createSlice({
       state.totalPrice = totalPrice;
       state.totalQuantity = totalQuantity;
     },
-    removeItem:(state,action)=>{
-        state.cart = state.cart.filter((item)=>item.id!==action.payload)
+    removeItem: (state, action) => {
+      state.cart = state.cart.filter((item) => item.id !== action.payload);
     },
-    incrementQuantity:(state,action)=>{
-        state.cart = state.cart.map((item)=>{
-            if(item.id===action.payload){
-                return {...item,quantity:item.productQuantity+1}
-            }
-            return item ; 
-        })
-    }
+    incrementItemQuantity: (state, action) => {
+      state.cart = state.cart.map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, productQuantity: item.productQuantity + 1 };
+        }
+        return item;
+      });
+    },
+    decreaseItemQuantity: (state, action) => {
+      state.cart = state.cart.map((item) => {
+        if (item.id === action.payload) {
+          if (item.productQuantity === 1) {
+            return null; // To remove the item from the cart if quantity becomes 0
+          }
+          return { ...item, productQuantity: item.productQuantity - 1 };
+        }
+        return item;
+      }).filter(Boolean);
+    },
   },
 });
 
-export const { addToCart, getCartTotal,removeItem,incrementQuantity } = cartSlice.actions;
+export const {
+  addToCart,
+  getCartTotal,
+  removeItem,
+  incrementItemQuantity,
+  decreaseItemQuantity,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -3,7 +3,8 @@ import NavBar from "../Components/NavBar";
 import { useSelector, useDispatch } from "react-redux";
 import { getCartTotal } from "../Features/cartSlice";
 import {removeItem} from '../Features/cartSlice'
-import {incrementQuantity} from '../Features/cartSlice'
+import {incrementItemQuantity} from '../Features/cartSlice'
+import {decreaseItemQuantity} from '../Features/cartSlice';
 
 const Cart = () => {
   const { cart, totalPrice, totalQuantity } = useSelector((state) => state.allcart);
@@ -15,46 +16,46 @@ const Cart = () => {
   }, [ cart]);
 
 
-  const handleRemove=(item)=>{
-    removeDispatch(removeItem(item))
-  }
   return (
     <div className="bg-gray-100 min-h-screen">
       <NavBar />
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-8">Your Shopping Cart</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {cart.map((item) => (
-            <div key={item.id} className="bg-white p-4 shadow-lg rounded-lg">
-              <img src={item.productImage} alt="Product Image" className="w-full" />
-              <div className="text-center mt-4">
-                <h2 className="text-lg font-bold">{item.productName}</h2>
-                <p className="text-sm text-gray-600">Price: ${item.productPrice}</p>
-                <div className="flex items-center justify-center mt-4">
-                  <button className="text-gray-600 hover:bg-gray-200 rounded-full p-1">-</button>
-                  <span className="mx-2">{item.productQuantity}</span>
-                  <button className="text-gray-600 hover:bg-gray-200 rounded-full p-1" onClick={dispatch(incrementQuantity(item.id))}>+</button>
-                </div>
-              </div>
-              <div className="text-center mt-4">
-                <button className="text-white bg-red-500 hover:bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5" onClick={()=>handleRemove(item.id)}>Remove</button>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="mt-8 flex justify-between">
+        {cart.length === 0 ? (
+          <h1 className="text-3xl font-bold mb-8">Your Shopping Cart is Empty !</h1>
+        ) : (
           <div>
-            <h2 className="text-lg font-bold">Order Summary</h2>
-            <p className="text-sm text-gray-600">Total Items: {totalQuantity}</p>
-            <p className="text-sm text-gray-600">Total Amount: ${totalPrice.toFixed(2)}</p>
+            <h1 className="text-3xl font-bold mb-8">Your Shopping Cart</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+              {cart.map((item) => (
+                <div key={item.id} className="bg-white p-4 shadow-lg rounded-lg">
+                  <img src={item.productImage} alt="Product Image" className="w-full" />
+                  <div className="text-center mt-4">
+                    <h2 className="text-lg font-bold">{item.productName}</h2>
+                    <p className="text-sm text-gray-600">Price: ${item.productPrice}</p>
+                    <div className="flex items-center justify-center mt-4">
+                      <button className="text-gray-600 hover:bg-gray-200 rounded-full p-1" onClick={() => dispatch(decreaseItemQuantity(item.id))}>-</button>
+                      <span className="mx-2">{item.productQuantity}</span>
+                      <button className="text-gray-600 hover:bg-gray-200 rounded-full p-1" onClick={() => dispatch(incrementItemQuantity(item.id))}>+</button>
+                    </div>
+                  </div>
+                  <div className="text-center mt-4">
+                    <button className="text-white bg-red-500 hover:bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5" onClick={() => dispatch(removeItem(item.id))}>Remove</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="mt-8 flex justify-between">
+              <div>
+                <h2 className="text-lg font-bold">Order Summary</h2>
+                <p className="text-sm text-gray-600"><strong>Total Items: {totalQuantity}</strong></p>
+                <p className="text-sm text-gray-600"><strong>Total Amount: ${totalPrice.toFixed(2)}</strong></p>
+              </div>
+              <button className="text-white bg-blue-500 hover:bg-blue-400 font-medium rounded-lg text-sm px-5 py-2.5">Proceed to Checkout</button>
+            </div>
           </div>
-          <button className="text-white bg-blue-500 hover:bg-blue-400 font-medium rounded-lg text-sm px-5 py-2.5">
-            Proceed to Checkout
-          </button>
-        </div>
+        )}
       </div>
     </div>
   );
 };
-
 export default Cart;
